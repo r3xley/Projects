fb221382c758667d2e8b086cf2b62a882b5cb2a7
r3xley <78571570+r3xley@users.noreply.github.com>
2023-01-05 19:11:49 -0500

    dis

---
+++ DiscordBotWebCovid/my_bot.py
@@ -0,0 +1,43 @@
+import discord
+import os
+import search_web
+
+
+web_run = search_web.RunWebScan()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+no_result_message =  'no'
+@client.event
+async def on_message(message):
+    if message.content == client.user:
+        return
+    
+    message_content = message.content.lower()
+
+    if message.content.startswith(f'$hello'):
+        await message.channel.send('''Hello there! I\'m the fidgeting bot from RunPee. 
+    Sorry but I really need to go to the bathroom... Please read my manual by typing $help or $commands while I'm away.''')
+
+        
+    if f'$search' in message_content:
+
+        key_words, search_words = web_run.key_words_search_words(message_content)
+        result_links = web_run.search(key_words)
+        links = web_run.send_link(result_links, search_words)
+    
+        if len(links) > 0:
+            for link in links:
+             await message.channel.send(link)
+        else:
+            await message.channel.send(no_result_message)
+        
+print(dir(web_run))
+
+
+
+
+
+client.run('MTA0NzcwNDUwNDg4OTUwNzkyMQ.GmyDNw.sDZSbFt5WffzYR7m1I-3Vq_RkdtM0VVZNj2n_Y')
---
+++ DiscordBotWebCovid/search_web.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+class RunWebScan:
+    def __init__(self):
+        self.url = 'https://coronavirus.jhu.edu/region/us/'
+
+    def key_words_search_words(self, user_message):
+        words = user_message.split()[1:]
+        keywords = '-'.join(words)
+        search_words = ' '.join(words)
+        #print(search_words)
+        return keywords, search_words
+    
+    def search(self, keywords):
+        # create requests and get the response
+    
+        response = requests.get(self.url+keywords)
+        print(response)
+        content = response.content
+        # pase the html and pull the data we want 
+        soup = BeautifulSoup(content, 'html.parser')
+        result_links = soup.findAll('a')
+        #print(result_links)
+        return result_links
+    
+    def send_link(self, result_links, search_words): 
+        send_link = set()
+        for link in result_links:
+            text = link.text.lower()
+            if search_words in text:  
+              send_link.add(link.get('href'))
+        return send_link
+
